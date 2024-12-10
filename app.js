a5215c241c65b37f34bb290d162e0949d3e2c594
Hussain Ali <iamhussainali2@gmail.com>
2024-12-10 16:40:37 +0530

    added dom

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+console.dir("Hello World")
+const demo = document.getElementById("demo")
+demo.style.backgroundColor="pink"
+console.log(demo)
+demo.innerText="Hello World"
+demo.innerHTML = `
+<div>
+<p class="hussain">Hussain</p>
+</div>
+`
